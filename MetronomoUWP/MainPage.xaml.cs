9398c53fd246e3df629b01446e9a96a4e445860b
BaezCrdrm <dev.baez_crdrm@outlook.com>
2016-10-28 21:48:23 -0500

    Avance del proyecto hasta la subida en GitHub

--- MetronomoUWP/MainPage.xaml.cs
+++ MetronomoUWP/MainPage.xaml.cs
@@ -15,29 +15,42 @@ using Windows.UI.Xaml.Navigation;
 
 using System.Threading;
 using System.Threading.Tasks;
-using Windows.System.Threading;
 
 namespace MetronomoUWP
 {
     public sealed partial class MainPage : Page
     {
         private bool isTimerRunning = false;
-        private int seconds = 0;
+        private int seconds = 0; //Se eliminará cuando funcione correctamente
         private CancellationTokenSource tokenSource;
+        public int bpm { get; set; }
 
         public MainPage()
         {
             this.InitializeComponent();
+            this.bpm = 60; //Cambiar en el futuro a un BPM usado previamente
         }
 
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
             seconds = 0;
             tokenSource = new CancellationTokenSource();
-            if(isTimerRunning == true)
+            if (isTimerRunning == true)
+            {
+                btnStart.Content = "Start";
+                txtBpm.IsEnabled = true;
+                btnLess.IsEnabled = true;
+                btnPlus.IsEnabled = true;
                 stopBackgroundTimer();
+            }
             else
+            {                
                 startBackgroundTimer();
+                txtBpm.IsEnabled = false;
+                btnLess.IsEnabled = false;
+                btnPlus.IsEnabled = false;
+                btnStart.Content = "Stop";
+            }
         }
 
         private void stopBackgroundTimer()
@@ -52,19 +65,19 @@ namespace MetronomoUWP
             }
         }
 
-        private async void startBackgroundTimer(int bpm = 60)
+        private async void startBackgroundTimer()
         {
             ///El código debe ejecutarse por un periodo indeterminado de tiempo.
             ///Basado en https://social.msdn.microsoft.com/Forums/windowsapps/en-US/40f4a4a9-1235-4071-b62b-e4077df3e1dc/uwpdispatch-timer-and-windows-10?forum=wpdevelop
             isTimerRunning = true;
-            int ts = (60/bpm) * 1000;
+            Int32 ts = (60/bpm) * 1000;
             TimeSpan myTimeSpan = new TimeSpan(0, 0, 0, 0, ts);
 
             while (this.isTimerRunning)
             {
                 /// Do something
                 /// Temporal
-                txbText.Text = seconds.ToString();
+                //txbText.Text = seconds.ToString();
                 System.Diagnostics.Debug.WriteLine(DateTime.Now);
                 /// End
 
@@ -84,7 +97,38 @@ namespace MetronomoUWP
                     break;
                 }
             }
-            this.isTimerRunning = false;
+            this.isTimerRunning = false;            
+        }
+
+        private void btnChangeBpm_Click(object sender, RoutedEventArgs e)
+        {
+            //Cambiar en el futuro para que sea asíncrono
+            //Se debe poder cambiar mientras está activado el metrónomo
+
+            if (((Button)sender).Name == btnLess.Name && this.bpm > 1)
+                this.bpm--;
+            else if(((Button)sender).Name == btnPlus.Name && this.bpm <= 200)
+                this.bpm++;
+
+            txtBpm.Text = this.bpm.ToString();
+        }
+
+        private void txtBpm_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //Arreglar
+            //if (isNumeric(txtBpm.Text) || txtBpm.Text == "")
+            //{
+            //    if (txtBpm.Text != "")
+            //        this.bpm = int.Parse(txtBpm.Text);
+            //}
+            //else
+            //    txtBpm.Text = this.bpm.ToString();
+        }
+
+        private bool isNumeric(string s)
+        {
+            int output;
+            return int.TryParse(s, out output);
         }
 
         ///Acerca de timers
